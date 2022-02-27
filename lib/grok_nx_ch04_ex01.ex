34a9db4fc45e1e286194e8db6b555e8ecbf9944b
Robin Hilliard <robin@rocketboots.com>
2022-02-28 07:45:32 +1100

    Leading 0s in file names

---
+++ lib/grok_nx_ch04_ex01.ex
@@ -0,0 +1,12 @@
+defmodule GrokNxCh4Ex1 do
+  @moduledoc """
+  Andrew W. Trask. “Grokking Deep Learning.” Manning Press
+  Chapter 4 example 1
+  
+  """
+  
+  import Nx.Defn
+  import Nx, only: :sigils
+
+
+end
