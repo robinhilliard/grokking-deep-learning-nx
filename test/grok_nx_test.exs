4883f218245b7fb61ed9918baab35662d79f3bb4
Robin Hilliard <robin@rocketboots.com>
2022-02-27 19:43:46 +1100

    Add sigils to example_1 example_2 passing tests

--- test/grok_nx_test.exs
+++ test/grok_nx_test.exs
@@ -2,6 +2,12 @@ defmodule GrokNxTest do
   use ExUnit.Case
 
   test "chapter_3_example_1" do
-    GrokNxCh3.example_1() == 0.98
+    assert GrokNxCh3.example_1() == 0.98
   end
+  
+  test "chapter_3_example_2" do
+    pred = GrokNxCh3.example_2()
+    assert ^pred = [0.213, 0.145, 0.506]
+  end
+  
 end
